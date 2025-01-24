8bb11a5e296bd8fe18af73af96e21a9b3c2d2158
neonaing2009 <neo.naing2009@gmail.com>
2025-01-24 06:11:53 +0000

    Appointment update module

--- customize/on_hospital/__manifest__.py
+++ customize/on_hospital/__manifest__.py
@@ -10,6 +10,7 @@
                 ],
     "data": [
         "security/ir.model.access.csv",
+        "data/sequence.xml",
         "views/patient_views.xml",
         "views/patient_readonly_views.xml",
         "views/appointment_views.xml",
--- customize/on_hospital/models/appointment.py
+++ customize/on_hospital/models/appointment.py
@@ -6,6 +6,14 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital Appointment'
     _rec_name = 'patient_id'
 
-    patient_id = fields.Many2one('hospital.patient', string="Patient")
-    date_appointment = fields.Date(string="Date Appointment")
-    note = fields.Text(string="Note")
+    reference = fields.Char(string='Reference',default='New', tracking=True)
+    patient_id = fields.Many2one('hospital.patient', string="Patient", tracking=True)
+    date_appointment = fields.Date(string="Date Appointment", tracking=True)
+    note = fields.Text(string="Note", tracking=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if not vals.get('reference') or vals.get('reference') == 'New':
+                vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
+        return super().create(vals_list)
