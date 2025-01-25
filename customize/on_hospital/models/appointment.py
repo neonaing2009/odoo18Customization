a99197b8f07b760aea3fd520ece147dd5c19f641
neonaing2009 <neo.naing2009@gmail.com>
2025-01-25 17:29:37 +0000

    Appointment Header Bar Button

--- customize/on_hospital/models/appointment.py
+++ customize/on_hospital/models/appointment.py
@@ -7,9 +7,14 @@ class HospitalAppointment(models.Model):
     _rec_name = 'patient_id'
 
     reference = fields.Char(string='Reference',default='New', tracking=True)
-    patient_id = fields.Many2one('hospital.patient', string="Patient", tracking=True)
+    patient_id = fields.Many2one('hospital.patient', string="Patient Name", tracking=True)
+    partner_id = fields.Many2one('res.partner', string="Sales Partner Name", tracking=True)
     date_appointment = fields.Date(string="Date Appointment", tracking=True)
     note = fields.Text(string="Note", tracking=True)
+    state = fields.Selection([
+        ('draft','Draft'),('confirmed','Confirmed'),('ongoing','Ongoing'),
+        ('done','Done'),('cancel','Cancelled')
+    ], default='draft', tracking=True)
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -17,3 +22,19 @@ class HospitalAppointment(models.Model):
             if not vals.get('reference') or vals.get('reference') == 'New':
                 vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
         return super().create(vals_list)
+
+    def action_confirm(self):
+        for rec in self:
+            rec.state = 'confirmed'
+
+    def action_ongoing(self):
+        for rec in self:
+            rec.state = 'ongoing'
+
+    def action_done(self):
+        for rec in self:
+            rec.state = 'done'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
